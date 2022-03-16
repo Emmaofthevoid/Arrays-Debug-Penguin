937975f06719dd0f94683c7ef35999f5af959679
emmaofthevoid <tehurapardo@outlook.es>
2022-03-16 15:39:33 +0100

    sdfgsh

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -9,8 +9,8 @@ public class ArrayDemo {
         sum = (arraySum[0] + arraySum2[0]) + (arraySum[1] + arraySum2[1]) + (arraySum[2] + arraySum2[2]);
         System.out.println(sum);
         for (int i = 0; i < arraySum.length; i++) {
-            System.out.print(arraySum[i] + arraySum2[i]);
-            System.out.print(",");
+            System.out.print(arraySum[i] + arraySum2[i] + ",");
+
 
         }
 
